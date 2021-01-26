4e823e4c95b9fc9cd2026e5345c6627d16f7bce1
NamelessLeaf <74725613+NamelessLeaf@users.noreply.github.com>
2021-01-26 14:11:12 +0000

    Update Main.php

--- src/NamelessLeaf/PvP/Main.php
+++ src/NamelessLeaf/PvP/Main.php
@@ -39,7 +39,7 @@ class Main extends PluginBase implements Listener {
   
   public function form($player){
     $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
-    $form = $api->createSimpleForm(function (Player $player, array $data = null){
+    $form = $fromapi->createSimpleForm(function (Player $player, array $data = null){
       $result = $data;
       if($result === null){
         return true;

5b90a97983f5c4776e19c1771edc0ae68ec60786
NamelessLeaf <74725613+NamelessLeaf@users.noreply.github.com>
2021-01-28 19:39:25 +0000

    Update Main.php

--- src/NamelessLeaf/PvP/Main.php
+++ src/NamelessLeaf/PvP/Main.php
@@ -36,14 +36,30 @@ class Main extends PluginBase implements Listener {
     }
     return true;
   }
-  public function form($player){
-    $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
-    $form = $api->createSimpleForm(function (Player $player, int $data = null){
-      $result = $data;
-      if($result === null){
-        return true;
-      }
-    });
-  }
-  
+    public function From($player){
+        $plugin = $this->getServer()->getPluginManager();
+        $formapi = $plugin->getPlugin("FormAPI");
+        $form = $formapi->createSimpleForm(function (Player $event, array $args){
+            $result = $args[0];
+            $player = $event->getPlayer();
+            if($result === null){
+            }
+            switch($result){
+                case 0:
+                    return;
+                case 1:
+                    $this->enableForm($player);
+                    return;
+                case 2:
+                    $this->disableForm($player);
+                    return;
+            }
+        });
+        $form->setTitle(TF::BOLD . TF::BLACK . "FlyUI Menu");
+        $form->setContent(TF::GRAY . "Enable/Disable Fly Mode!");
+        $form->addButton(TF::GREEN . "Enable");
+        $form->addButton(TF::RED . "Disable");
+        $form->addButton(TF::WHITE . "Cancel");
+        $form->sendToPlayer($player);
+    }  
 }

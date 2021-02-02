b8916bca2365b7a7b3cd366b982eb865bfaa2243
NamelessLeaf <74725613+NamelessLeaf@users.noreply.github.com>
2021-02-02 17:56:34 +0000

    Update Main.php

--- src/NamelessLeaf/PvP/Main.php
+++ src/NamelessLeaf/PvP/Main.php
@@ -53,7 +53,7 @@ class Main extends PluginBase implements Listener {
         break;
           
         case 1:
-         $player->transfer("endergames.ddns.net", 25331);
+         $player->transfer(""172.96.172.82, 27198);
         break;
       }
     });
